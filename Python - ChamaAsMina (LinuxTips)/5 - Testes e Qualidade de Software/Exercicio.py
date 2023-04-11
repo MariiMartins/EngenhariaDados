b97d3be0a4edb6970fddd8ab6ee2bd631a48e8b9
Mariana Martins <42555083+MariiMartins@users.noreply.github.com>
2023-04-11 18:50:51 -0300

    Atualização

---
+++ Python - ChamaAsMina (LinuxTips)/5 - Testes e Qualidade de Software/Exercicio.py
@@ -0,0 +1,21 @@
+import os
+import pytest
+from dundie.core import load
+from .constants import PEOPLE_FILE
+EXPECTED_NAMES = ["Jim Halpert", "Dwight Schrute"]
+EXPECTED_DEPTS = ["Sales"]
+EXPECTED_ROLES = ["Salesman", "Manager"]
+EMAIL_DOMAIN = "dundlermifflin.com"
+@pytest.mark.unit
+@pytest.mark.high
+def test_load(request
+             ):
+    """Test loaded data conforms with loaded file"""
+    result = load(PEOPLE_FILE) # load people.csv as a list of csv lines
+    assert len(result) == 3
+    for line in result:
+        data =  line.split( "," )
+        assert data[0] in EXPECTED_NAMES
+        assert data[1] in EXPECTED_DEPTS
+        assert data[2] in EXPECTED_ROLES
+        assert data[3] in EMAIL_DOMAIN
